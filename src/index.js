bb7905a16339cd65d4b2dd20f9aca08fcc350825
prabhat60343 <126355394+prabhat60343@users.noreply.github.com>
2025-01-23 21:28:03 +0530

    implement user registration and file upload functionality

--- src/index.js
+++ src/index.js
@@ -1,18 +1,16 @@
 // require('dotenv').config({path: './env'})
 import dotenv from "dotenv"
 import connectDB from "./db/index.js";
-import express from "express";
-const app = express();
-
+import {app} from './app.js'
 dotenv.config({
-    path: './env'
+    path: './.env'
 })
 
 
 
 connectDB()
 .then(() => {
-    app.listen(process.env.PORT || 8000, () => {
+    app.listen(process.env.PORT || 8001, () => {
         console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
     })
 })
@@ -21,3 +19,33 @@ connectDB()
 })
 
 
+
+
+
+
+
+
+
+
+/*
+import express from "express"
+const app = express()
+( async () => {
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        app.on("errror", (error) => {
+            console.log("ERRR: ", error);
+            throw error
+        })
+
+        app.listen(process.env.PORT, () => {
+            console.log(`App is listening on port ${process.env.PORT}`);
+        })
+
+    } catch (error) {
+        console.error("ERROR: ", error)
+        throw err
+    }
+})()
+
+*/
--- src/utils/apiError.js
+++ src/utils/apiError.js
@@ -10,7 +10,7 @@ class ApiError extends Error {
         this.data = null;
         this.message = message;
         this.success = false;
-        this.error = error;
+        this.error = errors;
 
         // Capture the stack trace or use the provided one
         if (stack) {
