54eca6ff74dcf89aff1857b28902040722f8eda3
prabhat60343 <126355394+prabhat60343@users.noreply.github.com>
2025-02-17 01:12:58 +0530

    Add new user account management routes for password change, account updates, avatar and cover image uploads, and watch history retrieval

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -3,7 +3,14 @@ import {
     loginUser, 
     logoutUser, 
     registerUser,
-    refreshAccessToken 
+    refreshAccessToken,
+    changeCurrentPassword, 
+    updateAccountDetails,
+    updateUserAvatar,
+    getWatchHistory,
+    updateUserCoverImage,
+    getCurrentUser,
+    getUserChannelProfile,
      
 } from "../controllers/user.controllers.js";
 
@@ -26,7 +33,23 @@ maxCount:1
     ,registerUser)
 
     router.route("/login").post(loginUser)
+
     router.route("/logout").post(verifyJWT, logoutUser)
+
     router.route("/refresh-token").post(refreshAccessToken)
+
+    router.route("/change-password").post(verifyJWT, changeCurrentPassword)
+
+    router.route("/current-user").get(verifyJWT, getCurrentUser)
+
+    router.route("/update-account").patch(verifyJWT, updateAccountDetails)
+
+    router.route("/avatar").patch(verifyJWT,upload.single("avatar"),updateUserAvatar)
+
+    router.route("/ cover-image").patch(verifyJWT, upload.single("/coverImage"),updateUserCoverImage)
+
+    router.route("/c/:username").get(verifyJWT,getUserChannelProfile)
+
+    router.route("/watchHistory").get(verifyJWT,getWatchHistory)
     
 export default router;
